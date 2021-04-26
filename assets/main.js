fe9296751008983ac745f86a0e46a24684c60818
alexellevu <newnapster83@gmail.com>
2021-04-26 16:26:25 +0200

    inizio esercizio

---
+++ assets/main.js
@@ -0,0 +1,5 @@
+//Chiedi all’utente la sua email
+var emailUtente = prompt('prego inserisci la tua e-mail');
+console.log(emailUtente);
+//controlla che sia nella lista di chi può accedere
+//stampa un messaggio appropriato sull’esito del controllo.
